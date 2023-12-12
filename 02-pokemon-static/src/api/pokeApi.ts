97d57d0ade6a4166354532efe74400f6cbf58999
joseespiritu <joseespiritu89@aragon.unam.mx>
2023-12-12 16:31:52 -0600

    ISG and ISR

--- 02-pokemon-static/src/api/pokeApi.ts
+++ 02-pokemon-static/src/api/pokeApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const pokeApi = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2'
+    baseURL: process.env.NEXT_PUBLIC_POKEMON_BASE_URL
 });
 
 export default pokeApi;
--- 02-pokemon-static/src/pages/name/[name].tsx
+++ 02-pokemon-static/src/pages/name/[name].tsx
@@ -90,7 +90,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemonNames.map((name) => ({
             params: { name }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 };
 
@@ -98,10 +98,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { name } = params as { name: string };
 
+    const pokemon = await getPokemonInfo(name);
+
+    if ( !pokemon ) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo(name)
-        }
+            pokemon
+        },
+        revalidate: 86400,
     }
 }
 
