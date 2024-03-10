b0cd7e0d32960e8a768c85d233fea1ab77d9d40e
joseespiritu <joseespiritu89@aragon.unam.mx>
2024-03-09 22:27:38 -0600

    teslo shop design pt2

--- 04-teslo-shop/src/app/(shop)/category/[id]/page.tsx
+++ 04-teslo-shop/src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,43 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProducts = initialData.products;
 
 interface Props {
     params: {
-        id: string
+        id: Category;
     }
 }
 
 export default function CategoryPage({params}: Props) {
 
     const { id } = params;
+    const products = seedProducts.filter( (product) => product.gender === id );
 
-    if (id === 'kids') {
-        notFound();
+    const labels: Record<Category, string> = {
+        'men': 'para Hombres',
+        'women': 'para Mujeres',
+        'kid': 'para Niños',
+        'unisex': 'para todos'
     }
 
+    // if (id === 'kids') {
+    //     notFound();
+    // }
+
     return (
-        <div>
-            <h1>Category Page</h1>
-        </div>
+        <>
+            <Title
+                title={`Artículos ${ labels[id] }`}
+                subtitle='Todos los productos'
+                className="mb-2"
+            />
+
+            <ProductGrid
+                products={products}
+            />
+        </>
     );
 }
---
+++ 04-teslo-shop/src/components/product/slideshow/ProductMobileSlideshow.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { Autoplay, FreeMode, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './slideshow.css';
+
+interface Props {
+    images: string[];
+    title: string;
+    className?: string;
+}
+
+export const ProductMobileSlideshow = ({ images, title, className }: Props) => {
+
+    return (
+        <div className={className}>
+            <Swiper
+                style={{
+                    width: '100vw',
+                    height: '500px'
+                }}
+                pagination={true}
+                autoplay={
+                    {
+                        delay: 2500
+                    }
+                }
+                modules={[FreeMode, Autoplay, Pagination]}
+                className="mySwiper2"
+            >
+                {
+                    images.map(image => (
+                        <SwiperSlide
+                            key={image}
+                        >
+                            <Image
+                                width={600}
+                                height={500}
+                                src={`/products/${image}`}
+                                alt={title}
+                                className='object-fill'
+                            />
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    )
+}
